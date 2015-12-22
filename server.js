e67f767864de3d12f27752c51a6cb3e79785a1ff
bethdianethomas <bethdianethomas@yahoo.com>
2015-12-22 01:03:44 -0700

    removing body parser and morgan dev

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var path = require('path');
 var http = require('http');
 var fs = require('fs');
-var bodyParser = require('body-parser');
-var morgan       = require('morgan');
+// var bodyParser = require('body-parser');
+// var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var session      = require('express-session');
 
@@ -13,10 +13,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(morgan('dev')); // log every request to the console
+// app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser.json()); // get information from html forms
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json()); // get information from html forms
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', function(req, res){

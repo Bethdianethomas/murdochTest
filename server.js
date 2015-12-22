865e7eb92785fed4b9cecb0537a36fb11ba30f99
bethdianethomas <bethdianethomas@yahoo.com>
2015-12-22 00:43:22 -0700

    trying to push to heroku

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var flash    = require('connect-flash');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+
+var app = express();
+
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.readFile('index.html');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Node app is running on port', app.get('port'));
+});
+
+
